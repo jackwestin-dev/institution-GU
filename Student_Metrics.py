831f904f969dd37deb6839907a286727eb1b4cfb
jackwestin-dev <163793998+jackwestin-dev@users.noreply.github.com>
2025-02-24 10:34:43 -0700

    Update Student_Metrics.py
    Engagement moved to top

--- Student_Metrics.py
+++ Student_Metrics.py
@@ -63,72 +63,6 @@ df_test_scores_student_filtered = df_test_scores[df_test_scores['student_id'] ==
 ## Create sections and render dashboard
 st.write(' ')
 st.write(' ')
-st.header('Accuracy')
-st.write(' ')
-st.write(' ')
-
-line_engagement = alt.Chart(df_engagement_attendance_student_filtered).mark_line(point=True).transform_fold(
-    fold=['sciences_accuracy', 'cars_accuracy','class_accuracy'], 
-    as_=['variable', 'value']
-).encode(
-    x=alt.X(
-        'week:O',
-        axis=alt.Axis(
-            labelAngle=0,
-            title='Week'
-        )
-    ),
-    y=alt.Y(
-        'value:Q',
-        axis=alt.Axis(
-            title='Accuracy Score',
-            format='%'
-        )
-    ),
-    tooltip=[
-        alt.Tooltip('week:O',title='Week'),
-        alt.Tooltip('value:Q',title='Accuracy Rate',format='0.1%')
-    ],
-    color=alt.Color('variable:N',legend=alt.Legend(title='Subject',orient='bottom'))
-)
-
-st.altair_chart(line_engagement,use_container_width=True)
-
-st.write(' ')
-st.write(' ')
-st.header('Attendance')
-st.write(' ')
-st.write(' ')
-
-line_attendance = alt.Chart(df_engagement_attendance_student_filtered).mark_line(point=True).transform_fold(
-    fold=['large_session','small_session'],
-    as_=['variable','value']
-).encode(
-    x=alt.X(
-        'week:O',
-        axis=alt.Axis(
-            labelAngle=0,
-            title='Week'
-        )
-    ),
-    y=alt.Y(
-        'value:Q',
-        axis=alt.Axis(
-            title='Cumulative Attendance Rate',
-            format='%'
-        )
-    ),
-    tooltip=[
-        alt.Tooltip('week:O',title='Week'),
-        alt.Tooltip('value:Q',title='Cumulative Attendance Rate',format='0.1%')
-    ],
-    color=alt.Color('variable:N',legend=alt.Legend(title='Session Size',orient='bottom'))
-)
-
-st.altair_chart(line_attendance,use_container_width=True)
-
-st.write(' ')
-st.write(' ')
 
 st.header('Engagement')
 st.subheader('Completed Lessons')
@@ -198,6 +132,70 @@ line_question_sets = alt.Chart(df_engagement_attendance_student_filtered).mark_l
 )
 
 st.altair_chart(line_question_sets,use_container_width=True)
+st.header('Accuracy')
+st.write(' ')
+st.write(' ')
+
+line_engagement = alt.Chart(df_engagement_attendance_student_filtered).mark_line(point=True).transform_fold(
+    fold=['sciences_accuracy', 'cars_accuracy','class_accuracy'], 
+    as_=['variable', 'value']
+).encode(
+    x=alt.X(
+        'week:O',
+        axis=alt.Axis(
+            labelAngle=0,
+            title='Week'
+        )
+    ),
+    y=alt.Y(
+        'value:Q',
+        axis=alt.Axis(
+            title='Accuracy Score',
+            format='%'
+        )
+    ),
+    tooltip=[
+        alt.Tooltip('week:O',title='Week'),
+        alt.Tooltip('value:Q',title='Accuracy Rate',format='0.1%')
+    ],
+    color=alt.Color('variable:N',legend=alt.Legend(title='Subject',orient='bottom'))
+)
+
+st.altair_chart(line_engagement,use_container_width=True)
+
+st.write(' ')
+st.write(' ')
+st.header('Attendance')
+st.write(' ')
+st.write(' ')
+
+line_attendance = alt.Chart(df_engagement_attendance_student_filtered).mark_line(point=True).transform_fold(
+    fold=['large_session','small_session'],
+    as_=['variable','value']
+).encode(
+    x=alt.X(
+        'week:O',
+        axis=alt.Axis(
+            labelAngle=0,
+            title='Week'
+        )
+    ),
+    y=alt.Y(
+        'value:Q',
+        axis=alt.Axis(
+            title='Cumulative Attendance Rate',
+            format='%'
+        )
+    ),
+    tooltip=[
+        alt.Tooltip('week:O',title='Week'),
+        alt.Tooltip('value:Q',title='Cumulative Attendance Rate',format='0.1%')
+    ],
+    color=alt.Color('variable:N',legend=alt.Legend(title='Session Size',orient='bottom'))
+)
+
+st.altair_chart(line_attendance,use_container_width=True)
+
 
 st.header('Participation')
 # st.write('The student has an aggregate weekly average rate of {class_participation:.1%} for class participation and {homework_participation:.1%} for homework participation. To date, overall participation is {overall_participation:.1%}.'.format(class_participation=class_participation,homework_participation=homework_participation,overall_participation=overall_participation))
